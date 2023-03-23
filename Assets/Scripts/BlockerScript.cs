caeccceca3701ae0f8acdc346e4220311e53e7cd
bhavishya7 <71849128+bhavishya7@users.noreply.github.com>
2023-03-23 04:01:35 -0500

    implemented blocking

--- Assets/Scripts/BlockerScript.cs
+++ Assets/Scripts/BlockerScript.cs
@@ -8,23 +8,27 @@ public class BlockerScript : MonoBehaviour
     public static int counterP1 = 0;
     public static int counterP2 = 0;
     public LayerMask Isles;
+    public bool toRend;
+    public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if(Input.GetMouseButtonDown(0) && counterP1 < 3 && SonicScript.turnP1){
+    {   
+        if(Input.GetMouseButtonDown(0) && SonicScript.turnP1 && counterP1 < 4){
+            rend.enabled = toRend;
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector3 offset = new Vector3(0,0,1);
             Instantiate(andrew, pos + offset, Quaternion.identity);
             counterP1 += 1;
         }
 
-         if(Input.GetMouseButtonDown(0) && counterP2 < 3  && SonicScriptP2.turnP2){
+         if(Input.GetMouseButtonDown(0) && SonicScriptP2.turnP2 && counterP2 < 4){
+            rend.enabled = toRend;
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector3 offset = new Vector3(0,0,1);
             Instantiate(andrew, pos + offset, Quaternion.identity);
